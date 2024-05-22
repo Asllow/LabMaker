57bb244755a03553ba1e5a939fe1d3d7800bb748
asllow <arthurmir0429@gmail.com>
2024-05-22 13:10:25 -0300

    teste 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -105,7 +105,7 @@
                             <span class="text-[15px] ml-4 text-gray-200 font-bold">Profile</span>
                         </div>
                     </a>
-                    <a href="">
+                    <a href="{{route('logout')}}">
                         <div class="my-4 bg-gray-600 h-[1px]"></div>
                         <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
                             <i class="bi bi-box-arrow-in-right"></i>

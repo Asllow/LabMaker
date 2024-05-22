feb2c6ef99aaf980cf4952687ac2db85aa81e130
asllow <arthurmir0429@gmail.com>
2024-05-22 13:15:56 -0300

    teste 2

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -85,6 +85,6 @@ class AuthController extends Controller
     public function logout(Request $request){
         Auth::guard('web')->logout();
         $request->session()->invalidate();
-        return redirect('welcome');
+        return redirect(route('welcome'));
     }
 }

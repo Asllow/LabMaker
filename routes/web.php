b8340c6b88db5a52be854dab5dea21390844e4e6
asllow <arthurmir0429@gmail.com>
2024-05-23 17:19:13 -0300

    teste 46

--- routes/web.php
+++ routes/web.php
@@ -35,10 +35,10 @@ Route::controller(PontoController::class)->group(function (){
         ->name('internetest');
 });
 
-Route::middleware(['auth', 'era-access:1'])->group(function () {
+Route::middleware(['auth', 'era-access:0'])->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 });
 
-Route::middleware(['auth', 'maker-access:0'])->group(function () {
+Route::middleware(['auth', 'maker-access:1'])->group(function () {
     Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
 });

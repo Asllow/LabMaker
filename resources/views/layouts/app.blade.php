3b353b96947327eb6959b967a7b16972fa89b1a1
asllow <arthurmir0429@gmail.com>
2024-05-23 16:01:50 -0300

    teste 46

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,7 +87,7 @@
                         <i class="bi bi-search text-sm"></i>
                         <input type="text" placeholder="Search" class="text-[15px] ml-4 w-full bg-transparent focus:outline-none" />
                     </div>
-                    <a href="{{ route('home') }}">
+                    <a href="{{ route('dashboard') }}">
                         <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
                             <i class="bi bi-house-door-fill"></i>
                             <span class="text-[15px] ml-4 text-gray-200 font-bold">Home</span>

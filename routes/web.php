daf1c25b2fe3c634160bd8a4ab894d0f3cc23ca8
asllow <arthurmir0429@gmail.com>
2024-05-23 17:27:19 -0300

    teste 46

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::controller(PontoController::class)->group(function (){
         ->name('internetest');
 });
 
-Route::middleware(['auth', 'maker-access:0, era-access:0, makesoft-access:0'])->group(function () {
+Route::middleware(['auth', 'maker-access:0', 'era-access:1'])->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 });
 

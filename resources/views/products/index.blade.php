bf3016b42ebcdae3a33724bbd90830bfe74dd464
asllow <arthurmir0429@gmail.com>
2024-05-24 15:05:36 -0300

    teste 46

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,11 +1,23 @@
 @extends('layouts.app')
 
-@section('title', 'Lista de Produtos')
+@section('title', 'Home Product List')
 
 @section('contents')
     <div>
-        <h1 class="font-bold text-2xl ml-3">Lista de Produtos</h1>
-        <a href="{{route('makesoft.produtos')}}" class="text-white float-right bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Adicionar Produto</a>
-        <hr/>
+        <h1 class="font-bold text-2xl ml-3">Home Product List</h1>
+        <a href="" class="text-white float-right bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add Product</a>
+        <hr />
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">#</th>
+                <th scope="col" class="px-6 py-3">Title</th>
+                <th scope="col" class="px-6 py-3">Price</th>
+                <th scope="col" class="px-6 py-3">Product Code</th>
+                <th scope="col" class="px-6 py-3">Description</th>
+                <th scope="col" class="px-6 py-3">Action</th>
+            </tr>
+            </thead>
+        </table>
     </div>
 @endsection

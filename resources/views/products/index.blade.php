786f29838a7ecf157466430fde30968458ea5f2c
asllow <arthurmir0429@gmail.com>
2024-05-24 16:36:32 -0300

    teste 46

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -19,6 +19,47 @@
                 <th scope="col" class="px-6 py-3">Action</th>
             </tr>
             </thead>
+            <tbody>
+            @if($product->count() > 0)
+                @foreach($product as $rs)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <th scope="row" class="font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $loop->iteration }}
+                        </th>
+                        <td>
+                            {{ $rs->img_produto }}
+                        </td>
+                        <td>
+                            {{ $rs->nome_produto }}
+                        </td>
+                        <td>
+                            {{ $rs->preco_produto }}
+                        </td>
+                        <td>
+                            {{ $rs->dimensao_produto }}
+                        </td>
+                        <td>
+                            {{ $rs->desc_produto }}
+                        </td>
+                        <td class="w-36">
+                            <div class="h-14 pt-5">
+                                <a href="" class="text-blue-800">Detail</a> |
+                                <a href="" class="text-green-800 pl-2">Edit</a> |
+                                <form action="" method="POST" onsubmit="return confirm('Delete?')" class="float-right text-red-800">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button>Delete</button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            @else
+                <tr>
+                    <td class="text-center" colspan="5">Product not found</td>
+                </tr>
+            @endif
+            </tbody>
         </table>
     </div>
 @endsection

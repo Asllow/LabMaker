1c2bde1e93e860a1a7bf715eb8558f5187789e1c
asllow <arthurmir0429@gmail.com>
2024-05-24 13:23:41 -0300

    teste 46

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,7 +93,7 @@
                             <span class="text-[15px] ml-4 text-gray-200 font-bold">Home</span>
                         </div>
                     </a>
-                    @if(auth()->user()->permission_maker >= '1')
+                    @if(auth()->user()->permission_makesoft >= '1')
                         <a href="">
                             <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
                                 <i class="bi bi-bookmark-fill"></i>

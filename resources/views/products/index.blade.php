833c5b1173966bab0a351115afd87df3ec7e9dad
asllow <arthurmir0429@gmail.com>
2024-05-24 14:52:19 -0300

    teste 46

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,7 +5,7 @@
 @section('contents')
     <div>
         <h1 class="font-bold text-2xl ml-3">Lista de Produtos</h1>
-        <a href="{{route('dashboard/produtos/criar')}}" class="text-white float-right bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Adicionar Produto</a>
+        <a href="{{route('dashboard/produtos/create')}}" class="text-white float-right bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Adicionar Produto</a>
         <hr/>
     </div>
 @endsection

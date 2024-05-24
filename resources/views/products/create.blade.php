739fc7bb8fc858f9da4fe5b4a05d884019e6025f
asllow <arthurmir0429@gmail.com>
2024-05-24 16:14:58 -0300

    teste 46

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -7,7 +7,7 @@
     <hr />
     <div class="border-b border-gray-900/10 pb-12">
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <form action="{{route('makesoft.produtos.store')}}" method="POST" enctype="multipart/form-data">
+            <form action="{{route('makesoft.produtos.store')}}" method="PUT" enctype="multipart/form-data">
                 @csrf
                 <div class="sm:col-span-4">
                     <label class="block text-sm font-medium leading-6 text-gray-900">Nome do Produto</label>

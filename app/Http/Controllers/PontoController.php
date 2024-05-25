a98b347e679b401050d97da7f529a3e397646606
asllow <arthurmir0429@gmail.com>
2024-05-25 18:37:31 -0300

    teste 46

--- app/Http/Controllers/PontoController.php
+++ app/Http/Controllers/PontoController.php
@@ -82,9 +82,14 @@ class PontoController extends Controller
     {
         $results_id_maker = IdMaker::where('io', 1)->get() ?? 0;
         if ($results_id_maker == []) {return "Saida Registrada 1";}
-
-        $results_id_maker->each->update(['io' => 0])->PunchClock::create(['registration' => $results_id_maker->registration, 'io' => 0]);
-
+        $size = sizeof($results_id_maker);
+        $results_id_maker->each->update(['io' => 0]);
+        for ($i = 0; $i < $size; $i++) {
+            PunchClock::create([
+                'registration' => $results_id_maker[$i]->registration,
+                'io' => '0'
+            ]);
+        }
         return "Saida Registrada";
     }
     private function getDateTime(): string

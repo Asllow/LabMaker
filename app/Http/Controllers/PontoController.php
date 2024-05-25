49c9a4a9dd0bb37189c16dbca809e89dc6628ee1
asllow <arthurmir0429@gmail.com>
2024-05-25 18:14:35 -0300

    teste 46

--- app/Http/Controllers/PontoController.php
+++ app/Http/Controllers/PontoController.php
@@ -80,14 +80,12 @@ class PontoController extends Controller
     private function removeRemnants(): string
     {
         $results_id_maker = IdMaker::where('io', 1)->get() ?? 0;
-        if (!$results_id_maker) {
-            return "Saida Registrada 1";
-        }
+        if (!$results_id_maker) {return "Saida Registrada 1";}
         $size = sizeof($results_id_maker);
         for ($i = 0; $i < $size; $i++) {
-            DB::update('update id_maker set io = 0 where registration = ?', array($registrations[$i]->registration));
+            DB::update('update id_maker set io = 0 where registration = ?', array($results_id_maker[$i]->registration));
             PunchClock::create([
-                'registration' => $registrations[$i]->registration,
+                'registration' => $results_id_maker[$i]->registration,
                 'io' => '0'
             ]);
         }

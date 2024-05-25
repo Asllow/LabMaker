fcf72054c5db8bc7c4c73ac93f05f3647f844c3a
asllow <arthurmir0429@gmail.com>
2024-05-25 18:30:00 -0300

    teste 46

--- app/Http/Controllers/PontoController.php
+++ app/Http/Controllers/PontoController.php
@@ -84,7 +84,7 @@ class PontoController extends Controller
         $results_id_maker = IdMaker::where('io', 1)->get() ?? 0;
         if ($results_id_maker == []) {return "Saida Registrada 1";}
         $size = sizeof($results_id_maker);
-        $results_id_maker->update(['io' => 0]);
+        $results_id_maker->each->update(['io' => 0]);
         for ($i = 0; $i < $size; $i++) {
             PunchClock::create([
                 'registration' => $results_id_maker[$i]->registration,

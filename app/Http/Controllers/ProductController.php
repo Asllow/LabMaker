e5adc3325ec3ca9043f53706b101f0b1abd2773d
asllow <arthurmir0429@gmail.com>
2024-05-25 12:57:36 -0300

    teste 46

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -31,7 +31,7 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         $request = $this->checkNull($request);
-        return $request->nome_produto;
+        return $request;
         Product::create($request->all());
         return redirect()->route('makesoft.produtos')->with('success', 'Produto cadastrado com sucesso!');
     }

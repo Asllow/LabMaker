2fd9ce005d754870c37ac7caf318cc56b66f1ae6
asllow <arthurmir0429@gmail.com>
2024-05-25 13:34:53 -0300

    teste 46

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,7 +8,7 @@
         <a href="{{route('makesoft.produtos.criar')}}" class="text-white float-right bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Adicionar Produtos</a>
         @if (session('success'))
             <div class="alert alert-success">
-                {{ session('status') }}
+                {{ session('success') }}
             </div>
         @endif
         <hr />

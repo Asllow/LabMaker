dea2f7c55f98a1a832f8c6d1d58143d28759f096
asllow <arthurmir0429@gmail.com>
2024-05-25 12:00:48 -0300

    teste 46

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,7 +5,12 @@
 @section('contents')
     <div>
         <h1 class="font-bold text-2xl ml-3">Lista de Produtos</h1>
-        <a href="{{route('makesoft.produtos.criar')}}" class="text-white float-right bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add Product</a>
+        <a href="{{route('makesoft.produtos.criar')}}" class="text-white float-right bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Adicionar Produtos</a>
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
         <hr />
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">

ecf3906994cbdbd53d98f45d3dc1125d5c9f31fc
asllow <arthurmir0429@gmail.com>
2024-05-25 17:50:29 -0300

    teste 46

--- app/Http/Controllers/PontoController.php
+++ app/Http/Controllers/PontoController.php
@@ -19,50 +19,13 @@ class PontoController extends Controller
         $this->checkAny($id);
         switch ($id) {
             case "1":
-                $this->checkAny($any);
-                $results_user = User::firstWhere('registration', $any) ?? 0;
-                if (!$results_user) {return 'Erro 1';}
-                return $results_user->registration . $results_user->name . " " . $results_user->last_name;
+                return $this->checkRegistration($any);
             case "2":
-                $this->checkAny($any);
-                $results_id_maker = IdMaker::firstWhere('hexa_id', $any) ?? 0;
-                if (!$results_id_maker) {return 'Erro 2';}
-                $results_user = User::firstWhere('registration', $results_id_maker->registration) ?? 0;
-                if (!$results_user ) {return 'Erro 3';}
-                return $results_id_maker->registration . $results_user ->name . " " . $results_user ->last_name;
+                return $this->checkHexaId($any);
             case "3":
-                $this->checkAny($any);
-                $results_id_maker = IdMaker::firstWhere('registration', $any) ?? 0;
-                if (!$results_id_maker) {return 'Erro 4';}
-                $io = match ($results_id_maker->io) {
-                    0 => 1,
-                    1 => 0,
-                };
-                $texto = match ($io) {
-                    0 => "Saida",
-                    1 => "Entrada",
-                };
-                $results_id_maker->update(['io' => $io]);
-                PunchClock::create([
-                    'registration' => $any,
-                    'io' => $io,
-                    'created_at' => $timestamp,
-                ]);
-                return "Inscrito|" . $texto;
+                return $this->pushInPunchClock($any, $timestamp);
             case "4":
-                $registrations = DB::select('select registration from id_maker where io = 1') ?? 0;
-                if (!$registrations) {
-                    return "Saida Registrada 1";
-                }
-                $size = sizeof($registrations);
-                for ($i = 0; $i < $size; $i++) {
-                    DB::update('update id_maker set io = 0 where registration = ?', array($registrations[$i]->registration));
-                    PunchClock::create([
-                        'registration' => $registrations[$i]->registration,
-                        'io' => '0'
-                    ]);
-                }
-                return "Saida Registrada";
+                return $this->removeRemnants();
             case "5":
                 return $this->getDateTime();
             case "6":
@@ -77,7 +40,59 @@ class PontoController extends Controller
             abort(404);
         }
     }
-
+    private function checkRegistration($any): string
+    {
+        $this->checkAny($any);
+        $results_user = User::firstWhere('registration', $any) ?? 0;
+        if (!$results_user) {return 'Erro 1';}
+        return $results_user->registration . $results_user->name . " " . $results_user->last_name;
+    }
+    private function checkHexaId($any): string
+    {
+        $this->checkAny($any);
+        $results_id_maker = IdMaker::firstWhere('hexa_id', $any) ?? 0;
+        if (!$results_id_maker) {return 'Erro 2';}
+        $results_user = User::firstWhere('registration', $results_id_maker->registration) ?? 0;
+        if (!$results_user ) {return 'Erro 3';}
+        return $results_id_maker->registration . $results_user ->name . " " . $results_user ->last_name;
+    }
+    private function pushInPunchClock($any, $timestamp): string
+    {
+        $this->checkAny($any);
+        $results_id_maker = IdMaker::firstWhere('registration', $any) ?? 0;
+        if (!$results_id_maker) {return 'Erro 4';}
+        $io = match ($results_id_maker->io) {
+            0 => 1,
+            1 => 0,
+        };
+        $texto = match ($io) {
+            0 => "Saida",
+            1 => "Entrada",
+        };
+        $results_id_maker->update(['io' => $io]);
+        PunchClock::create([
+            'registration' => $any,
+            'io' => $io,
+            'created_at' => $timestamp,
+        ]);
+        return "Inscrito|" . $texto;
+    }
+    private function removeRemnants(): string
+    {
+        $registrations = DB::select('select registration from id_maker where io = 1') ?? 0;
+        if (!$registrations) {
+            return "Saida Registrada 1";
+        }
+        $size = sizeof($registrations);
+        for ($i = 0; $i < $size; $i++) {
+            DB::update('update id_maker set io = 0 where registration = ?', array($registrations[$i]->registration));
+            PunchClock::create([
+                'registration' => $registrations[$i]->registration,
+                'io' => '0'
+            ]);
+        }
+        return "Saida Registrada";
+    }
     private function getDateTime(): string
     {
         $currentDateTime = new DateTime('now');

c9abdd17ba4211328ba7862fa879b9fe1956c03b
asllow <arthurmir0429@gmail.com>
2024-05-25 18:01:19 -0300

    teste 46

--- app/Http/Controllers/PontoController.php
+++ app/Http/Controllers/PontoController.php
@@ -79,7 +79,7 @@ class PontoController extends Controller
     }
     private function removeRemnants(): string
     {
-        $results_id_maker = IdMaker::where('io', 1) ?? 0;
+        $results_id_maker = IdMaker::where('io', 1)->take(10) ?? 0;
         if (!$results_id_maker) {
             return "Saida Registrada 1";
         }

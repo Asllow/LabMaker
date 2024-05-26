29c2df0c75951eedf1758f1c7d050cffee24d8fe
asllow <arthurmir0429@gmail.com>
2024-05-25 22:21:38 -0300

    teste 46

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        {{-- Configuration --}}
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,9 +11,13 @@
     <body>
         <header>
             <nav>
-
+                <div>
+                    <a href="{{route('welcome')}}"><strong>Lab<strong>Maker</strong></strong></a>
+                </div>
+                <div></div>
             </nav>
         </header>
         @yield('main')
     </body>
 </html>
+

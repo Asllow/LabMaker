78508eda8479bff6b0d850fcee68893ba3436c0d
asllow <arthurmir0429@gmail.com>
2024-05-25 21:02:10 -0300

    teste 46

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>@yield('title')</title>
-</head>
-
-<body>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>@yield('title')</title>
+    </head>
+    <body></body>
 </html>

834d0ad7c654bf1b034fb182fb60687f6575367e
asllow <arthurmir0429@gmail.com>
2024-05-27 16:49:55 -0300

    teste 46

--- public/js/app.js
+++ public/js/app.js
@@ -13,27 +13,3 @@ for (const link of links) {
         nav.classList.remove('show')
     })
 }
-//Selecionar a Página
-const sections = document.querySelectorAll('body[id]')
-function activateMenuAtCurrentSection() {
-    for (const section of sections) {
-        const sectionId = section.getAttribute('id')
-        document
-            .querySelector('nav ul li a[id*=' + sectionId + ']')
-            .classList.add('active')
-    }
-}
-// Botão voltar para o topo
-const backToTopButton = document.querySelector('.back-to-top')
-function backToTop() {
-    if (window.scrollY >= 360) {
-        backToTopButton.classList.add('show')
-    } else {
-        backToTopButton.classList.remove('show')
-    }
-}
-
-activateMenuAtCurrentSection()
-window.addEventListener('scroll', function () {
-    backToTop()
-})
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,6 +8,7 @@
         <meta name="description" content="@yield('description')">
         <title>@yield('title')</title>
         <link href="{{asset("css/app.css")}}" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         {{-- Favicon --}}
         <link rel="apple-touch-icon" sizes="180x180" href="{{asset("favicon/apple-touch-icon.png")}}">
         <link rel="icon" type="image/png" sizes="32x32" href="{{asset("favicon/favicon-32x32.png")}}">
@@ -17,7 +18,6 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
-
     </head>
     <body>
         <header id='header'>

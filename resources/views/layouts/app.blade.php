3dcbd3daa911bd6d3087f81dfb578a5bde24f80d
asllow <arthurmir0429@gmail.com>
2024-05-26 21:13:48 -0300

    teste 46

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+        <link href="{{asset("css/app.css")}}" rel="stylesheet" />
     </head>
 
     <body>

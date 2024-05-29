bed138bcd8e73dc421a07210a70ea2d19f2ab3e2
asllow <arthurmir0429@gmail.com>
2024-05-29 13:56:40 -0300

    teste 46

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -34,7 +34,7 @@
                         @auth()
                             <li><a class='title' id='dashboard' href='{{route('dashboard')}}'>Dashboard</a></li>
                             <div class="separator"></div>
-                            <div data-dropdown class='perfil' type="button" x-data="{ open: false }" @click="open = true" :class="{ 'bg-gray-200 rounded-md': open }">
+                            <div class="flex items-center px-3 py-2 focus:outline-none hover:bg-gray-200 hover:rounded-md" data-dropdown class='perfil' type="button" x-data="{ open: false }" @click="open = true" :class="{ 'bg-gray-200 rounded-md': open }">
                                 <h2 class='title'><div class="outline">{{substr(auth()->user()->name, 0, 1)}}</div></h2>
                                 <h2 class='subtitle'>{{auth()->user()->name}}</h2>
                                 <div data-dropdown-items class="text-sm text-left absolute top-0 right-0 mt-16 mr-4 bg-white rounded border border-gray-400 shadow" x-show="open" @click.away="open = false">
@@ -50,9 +50,6 @@
                             <li><a class='title' id='entrar' href='{{route('entrar')}}'>Entrar</a></li>
                             <a class='bi bi-person-circle' href='{{route('entrar')}}'></a>
                         @endguest
-                        <button  class="flex items-center px-3 py-2 focus:outline-none hover:bg-gray-200 hover:rounded-md" >
-
-                        </button>
                         <div class='triangulo'>
                             <a class='logo' href='{{route('welcome')}}'>
                                 <img src='{{asset("img/icons/labmaker_lampada.svg")}}' alt="Logo">

2ea497d52953696945f8f8e2ba2234106ef1dc13
asllow <arthurmir0429@gmail.com>
2024-10-25 17:51:51 -0300

    Teste01

--- app/Http/Controllers/PontoController.php
+++ app/Http/Controllers/PontoController.php
@@ -19,7 +19,7 @@ class PontoController extends Controller
     {
         $results_all = PunchClock::where('registration', $registration)->get() ?? 0;
         $results_query = PunchClock::where('registration', $registration)->whereYear('created_at', $date)->get() ?? 0;
-        return $this->get_hours($results_query) . " - " . $this->get_hours($results_all);
+        return $this->get_hours($results_query);
     }
 
     public function ponto(string $id, string $any, string $timestamp)

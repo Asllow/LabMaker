615c82e4cd7e2f856314ee4f10416b666f96acb3
asllow <arthurmir0429@gmail.com>
2024-10-25 18:19:33 -0300

    Teste01

--- resources/views/hours/index.blade.php
+++ resources/views/hours/index.blade.php
@@ -1,6 +1,6 @@
 <h1>Contador de horas trabalhadas - LabMaker</h1>
-<h2 style="margin-top: 1rem; margin-left: 1rem">{{ $user->registration }} - {{ $user->name }} {{ $user->last_name }}</h2>
-<div style="display: inline-flex; gap: 2rem; margin-top: 1rem; margin-left: 1rem">
+<h2 style="margin-top: 2rem; margin-left: 1rem">{{ $user->registration }} - {{ $user->name }} {{ $user->last_name }}</h2>
+<div style="display: inline-flex; gap: 2rem; margin-top: 0.25rem; margin-left: 1rem">
     <div>
         <fieldset>
             <legend><h4>Horas Totais</h4></legend>

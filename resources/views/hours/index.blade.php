5c77d11451801639387f1c69afee870bb02a9edf
asllow <arthurmir0429@gmail.com>
2024-10-25 18:18:25 -0300

    Teste01

--- resources/views/hours/index.blade.php
+++ resources/views/hours/index.blade.php
@@ -1,16 +1,16 @@
 <h1>Contador de horas trabalhadas - LabMaker</h1>
-<h2 style="margin-top: 1rem">{{ $user->registration }} - {{ $user->name }} {{ $user->last_name }}</h2>
+<h2 style="margin-top: 1rem; margin-left: 1rem">{{ $user->registration }} - {{ $user->name }} {{ $user->last_name }}</h2>
 <div style="display: inline-flex; gap: 2rem; margin-top: 1rem; margin-left: 1rem">
     <div>
         <fieldset>
-            <h4>Horas Totais</h4>
-            <strong>{{ $hours_all }}</strong>
+            <legend><h4>Horas Totais</h4></legend>
+            <strong>{{ $hours_all }} horas</strong>
         </fieldset>
     </div>
     <div>
         <fieldset>
-            <h4>Horas do ano escolhido ({{ $date }})</h4>
-            <strong>{{ $hours_query }}</strong>
+            <legend><h4>Horas do ano escolhido ({{ $date }})</h4></legend>
+            <strong>{{ $hours_query }} horas</strong>
         </fieldset>
     </div>
 </div>

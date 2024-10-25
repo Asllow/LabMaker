32bfcecced615a998cbaa076fd9080c3dd0fce2a
asllow <arthurmir0429@gmail.com>
2024-10-25 18:07:34 -0300

    Teste01

--- app/Http/Controllers/PontoController.php
+++ app/Http/Controllers/PontoController.php
@@ -17,9 +17,12 @@ class PontoController extends Controller
 
     public function gethours(string $registration, string $date)
     {
+        $user = User::firstWhere('registration', $registration) ?? 0;
         $results_all = PunchClock::where('registration', $registration)->get() ?? 0;
         $results_query = PunchClock::where('registration', $registration)->whereYear('created_at', $date)->get() ?? 0;
-        return $this->get_hours($results_query) . " - " . $this->get_hours($results_all);
+        $hours_all = $this->get_hours($results_all);
+        $hours_query = $this->get_hours($results_query);
+        return view('hours.index', compact('hours_all', 'hours_query', 'user'));
     }
 
     public function ponto(string $id, string $any, string $timestamp)
---
+++ resources/views/hours/index.blade.php
@@ -0,0 +1,2 @@
+<h1>Contador de horas trabalhadas</h1>
+<h3>{{ $user->registration }}</h3>

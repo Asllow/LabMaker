8c812cf242761cc4d5c8660f53d4a9778a0abb0b
asllow <arthurmir0429@gmail.com>
2024-10-25 16:19:26 -0300

    Teste01

--- app/Http/Controllers/PontoController.php
+++ app/Http/Controllers/PontoController.php
@@ -17,12 +17,6 @@ class PontoController extends Controller
     {
         $results_all = PunchClock::where('registration', $registration)->get() ?? 0;
         $results_query = PunchClock::where('registration', $registration)->whereYear('created_at', $date)->get() ?? 0;
-        if ($this->check(count($results_all)) == 1){
-            array_pop($results_all);
-        }
-        if ($this->check(count($results_query)) == 1){
-            array_pop($results_query);
-        }
         $results_year = [];
         foreach ($results_query as $result){
             echo $result->created_at;

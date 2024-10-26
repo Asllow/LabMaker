03740d8be8db36af8eca5d9938c12e21c1a607fa
asllow <arthurmir0429@gmail.com>
2024-10-26 16:56:53 -0300

    Teste01

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,10 +19,10 @@
                             <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                             </svg>
-                            <span class="ml-2 text-sm font-medium">Products</span>
+                            <span class="ml-2 text-sm font-medium">Produtos</span>
                         </a>
                     @endif
-                    <a class="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" href="#">
+                    <a class="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" href="{{route('dashboard.perfil')}}">
                         <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>

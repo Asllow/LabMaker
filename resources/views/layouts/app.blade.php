9158d4d8404a6e2425ed9ab3ee5a017c483dcc41
asllow <arthurmir0429@gmail.com>
2024-10-26 15:32:09 -0300

    Teste01

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,49 +2,45 @@
 
 @section('main')
     <div class="flex flex-row">
-        <div class="flex flex-col w-64 h-screen overflow-y-auto bg-gray-900 border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
-            <div class="sidebar text-center bg-gray-900">
-                <div class="text-gray-100 text-xl">
-                    <div class="p-2.5 mt-1 flex items-center">
-                        <i class="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600"></i>
-                        <h1 class="font-bold text-gray-200 text-[15px] ml-3">Admin</h1>
-                    </div>
-                    <div class="my-2 bg-gray-600 h-[1px]"></div>
+        <div>@yield('navbar')</div>
+        {{--<div class="">
+            <div class="text-gray-100 text-xl">
+                <div class="p-2.5 mt-1 flex items-center">
+                    <i class="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600"></i>
+                    <h1 class="font-bold text-gray-200 text-[15px] ml-3">Admin</h1>
                 </div>
-                <div class="p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-gray-700 text-white">
-                    <i class="bi bi-search text-sm"></i>
-                    <input type="text" placeholder="Search" class="text-[15px] ml-4 w-full bg-transparent focus:outline-none" />
+                <div class="my-2 bg-gray-600 h-[1px]"></div>
+            </div>
+            <a href="{{ route('dashboard') }}">
+                <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                    <i class="bi bi-house-door-fill"></i>
+                    <span class="text-[15px] ml-4 text-gray-200 font-bold">Início</span>
                 </div>
-                <a href="{{ route('dashboard') }}">
-                    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-                        <i class="bi bi-house-door-fill"></i>
-                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Início</span>
-                    </div>
-                </a>
-                @if(auth()->user()->permission_makesoft >= '1')
-                    <a href="{{route('makesoft.produtos')}}">
-                        <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-                            <i class="bi bi-bookmark-fill"></i>
-                            <span class="text-[15px] ml-4 text-gray-200 font-bold">Produtos</span>
-                        </div>
-                    </a>
-                @endif
-                <a href="{{route('dashboard.perfil')}}">
+            </a>
+            @if(auth()->user()->permission_makesoft >= '1')
+                <a href="{{route('makesoft.produtos')}}">
                     <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
                         <i class="bi bi-bookmark-fill"></i>
-                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Perfil</span>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Produtos</span>
                     </div>
                 </a>
-                <a href="{{route('sair')}}">
-                    <div class="my-4 bg-gray-600 h-[1px]"></div>
-                    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-                        <i class="bi bi-box-arrow-in-right"></i>
-                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Sair</span>
-                    </div>
-                </a>
-            </div>
+            @endif
+            <a href="{{route('dashboard.perfil')}}">
+                <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                    <i class="bi bi-bookmark-fill"></i>
+                    <span class="text-[15px] ml-4 text-gray-200 font-bold">Perfil</span>
+                </div>
+            </a>
+            <a href="{{route('sair')}}">
+                <div class="my-4 bg-gray-600 h-[1px]"></div>
+                <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                    <i class="bi bi-box-arrow-in-right"></i>
+                    <span class="text-[15px] ml-4 text-gray-200 font-bold">Sair</span>
+                </div>
+            </a>
         </div>
-        <div class="flex flex-col w-full h-screen px-4 py-8 mt-10">
+        --}}
+        <div class="">
             <div>@yield('contents')</div>
         </div>
     </div>

382faf8e6bd264e8c59026ea5b7e5cbc84f396df
asllow <arthurmir0429@gmail.com>
2024-10-29 15:36:28 -0300

    Teste01

--- resources/views/auth/cadastrar.blade.php
+++ resources/views/auth/cadastrar.blade.php
@@ -195,12 +195,14 @@
             x = document.getElementsByClassName("tab");
             y = x[currentTab].getElementsByTagName("input");
             // A loop that checks every input field in the current tab:
-            for (i = 0; i < y.length; i++) {
-                // If a field is empty...
-                if (y[i].value === "") {
-                    // add an "invalid" class to the field:
-                    // and set the current valid status to false:
-                    valid = false;
+            if (currentTab !== 1){
+                for (i = 0; i < y.length; i++) {
+                    // If a field is empty...
+                    if (y[i].value === "") {
+                        // add an "invalid" class to the field:
+                        // and set the current valid status to false:
+                        valid = false;
+                    }
                 }
             }
             // If the valid status is true, mark the step as finished and valid:

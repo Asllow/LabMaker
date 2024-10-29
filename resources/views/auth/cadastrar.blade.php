4cd3c1a1b659024748c13416470753053a14db9e
asllow <arthurmir0429@gmail.com>
2024-10-29 12:22:14 -0300

    Teste01

--- resources/views/auth/cadastrar.blade.php
+++ resources/views/auth/cadastrar.blade.php
@@ -42,86 +42,87 @@
                                 @enderror
                             </div>
                         </div>
-                        <div>
-                            <label for="registration" class="">Número de matrícula</label>
-                            <input type="tel" name="registration" id="registration" class="" placeholder="Sua matricula"  maxlength="11" pattern="[0-9]{11}" autocomplete="off" value="{{ old('registration') }}">
-                        </div>
-                        <div class="">
-                            <div class="">
-                                <input type="checkbox" class='checkbox' id="registrationfalse" name="registrationfalse" aria-describedby="registrationfalse" class="">
+                        <div class="tab">
+                            <div>
+                                <label for="registration" class="">Número de matrícula</label>
+                                <input type="tel" name="registration" id="registration" class="" placeholder="Sua matricula"  maxlength="11" pattern="[0-9]{11}" autocomplete="off" value="{{ old('registration') }}">
                             </div>
                             <div class="">
-                                <label for="registrationfalse" class="">
-                                    Não possuo número de matrícula
-                                </label>
+                                <div class="">
+                                    <input type="checkbox" class='checkbox' id="registrationfalse" name="registrationfalse" aria-describedby="registrationfalse" class="">
+                                </div>
+                                <div class="">
+                                    <label for="registrationfalse" class="">
+                                        Não possuo número de matrícula
+                                    </label>
+                                </div>
+                                @error('registration')
+                                <span class="text-red-600">{{ $message }}</span>
+                                @enderror
                             </div>
-                            @error('registration')
-                            <span class="text-red-600">{{ $message }}</span>
-                            @enderror
+                            <div>
+                                <label for="course" class="">Seu curso</label>
+                                <select name="course" id="course" required>
+                                    <option selected disabled>Selecione seu curso</option>
+                                    <option value="1">Engenharia Mecatrônica</option>
+                                    <option value="2">Engenharia de Computação</option>
+                                    <option value="3">Design de Moda</option>
+                                    <option value="4">Téc. em Mecatrônica</option>
+                                    <option value="5">Téc. em Informática</option>
+                                    <option value="6">Téc. em Produção de Moda</option>
+                                    <option value="7">Téc. em Eletromecânica</option>
+                                    <option value="8">Téc. em Informática para internet</option>
+                                    <option value="9">Servidor do CEFET</option>
+                                    <option value="0">Não tenho curso</option>
+                                </select>
+                                @error('course')
+                                <span class="text-red-600">{{ $message }}</span>
+                                @enderror
+                            </div>
                         </div>
-                        <div>
-                            <label for="course" class="">Seu curso</label>
-                            <select name="course" id="course" required>
-                                <option selected disabled>Selecione seu curso</option>
-                                <option value="1">Engenharia Mecatrônica</option>
-                                <option value="2">Engenharia de Computação</option>
-                                <option value="3">Design de Moda</option>
-                                <option value="4">Téc. em Mecatrônica</option>
-                                <option value="5">Téc. em Informática</option>
-                                <option value="6">Téc. em Produção de Moda</option>
-                                <option value="7">Téc. em Eletromecânica</option>
-                                <option value="8">Téc. em Informática para internet</option>
-                                <option value="9">Servidor do CEFET</option>
-                                <option value="0">Não tenho curso</option>
-                            </select>
-                            @error('course')
-                            <span class="text-red-600">{{ $message }}</span>
-                            @enderror
+                        <div class="tab">
+                            <div>
+                                <label for="birth_date" class="">Data de Nascimento</label>
+                                <input type="date" name="birth_date" id="birth_date" placeholder="Sua data de nascimento" class="" required="" value="{{ old('birth_date') }}">
+                                @error('birth_date')
+                                <span class="text-red-600">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div>
+                                <label for="phone" class="">Telefone</label>
+                                <input onkeydown="phoneNumberFormatter()" type="tel" name="phone" id="phone" placeholder="(99) 99999-9999" class="" maxlength="15" pattern="(\([0-9]{2}\))\s([9]{1})?([0-9]{5})-([0-9]{4})" autocomplete="off" value="{{ old('phone') }}">
+                                @error('phone')
+                                <span class="text-red-600">{{ $message }}</span>
+                                @enderror
+                            </div>
                         </div>
-                        <div>
-                            <label for="birth_date" class="">Data de Nascimento</label>
-                            <input type="date" name="birth_date" id="birth_date" placeholder="Sua data de nascimento" class="" required="" value="{{ old('birth_date') }}">
-                            @error('birth_date')
-                            <span class="text-red-600">{{ $message }}</span>
-                            @enderror
-                        </div>
-                        <div>
-                            <label for="phone" class="">Telefone</label>
-                            <input onkeydown="phoneNumberFormatter()" type="tel" name="phone" id="phone" placeholder="(99) 99999-9999" class="" maxlength="15" pattern="(\([0-9]{2}\))\s([9]{1})?([0-9]{5})-([0-9]{4})" autocomplete="off" value="{{ old('phone') }}">
-                            @error('phone')
-                            <span class="text-red-600">{{ $message }}</span>
-                            @enderror
-                        </div>
-                        <div>
-                            <label for="password" class="">Senha</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" class="" required="">
-                            @error('password')
-                            <span class="text-red-600">{{ $message }}</span>
-                            @enderror
-                        </div>
-                        <div>
-                            <label for="password_confirmation" class="">Confirme a senha</label>
-                            <input type="password" name="password_confirmation" id="password_confirmation" placeholder="••••••••" class="" required="">
-                            @error('password_confirmation')
-                            <span class="text-red-600">{{ $message }}</span>
-                            @enderror
-                        </div>
-                        <div class="">
+                        <div class="tab">
+                            <div>
+                                <label for="password" class="">Senha</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" class="" required="">
+                                @error('password')
+                                <span class="text-red-600">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div>
+                                <label for="password_confirmation" class="">Confirme a senha</label>
+                                <input type="password" name="password_confirmation" id="password_confirmation" placeholder="••••••••" class="" required="">
+                                @error('password_confirmation')
+                                <span class="text-red-600">{{ $message }}</span>
+                                @enderror
+                            </div>
                             <div class="">
-                                <input id="terms" aria-describedby="terms" type="checkbox" class="checkbox" required="">
-                            </div>
-                            <div class="ml-3 text-sm">
-                                <label for="terms" class="">
-                                    Eu aceito os
-                                    <a class="" href="#">
-                                        Termos e Condições
-                                    </a>
-                                </label>
+                                <div class="">
+                                    <input id="terms" aria-describedby="terms" type="checkbox" class="checkbox" required="">
+                                </div>
+                                <div class="ml-3 text-sm">
+                                    <label for="terms" class="">
+                                        Eu aceito os<a class="" href="#">Termos e Condições</a>
+                                    </label>
+                                </div>
                             </div>
                         </div>
-                        <button type="submit" class="">
-                            Criar uma conta
-                        </button>
+                        <button type="submit" class="">Criar uma conta</button>
                     </form>
                 </div>
             </div>

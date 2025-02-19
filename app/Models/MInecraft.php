1c2954fe0f139e5b97a6861e2c4721a0431bec36
asllow <arthurmir0429@gmail.com>
2025-02-19 17:51:53 -0300

    Teste01

--- app/Models/MInecraft.php
+++ app/Models/MInecraft.php
@@ -9,6 +9,10 @@ class MInecraft extends Model
 {
     protected $table = 'minecraft';
 
+    public $timestamps = [];
+    const UPDATED_AT = null;
+    const CREATED_AT = null;
+
     use HasFactory;
 
     protected $fillable = [
